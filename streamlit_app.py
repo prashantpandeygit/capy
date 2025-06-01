f83c23c673a0d300904081c2ab4091d46487938b
Prashant Pandey <132379659+prashantpandeygit@users.noreply.github.com>
2025-06-01 23:00:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,23 +13,32 @@ def load_model():
 
 model, processor, tokenizer = load_model()
 
-# Caption generation function
+# Caption generation function (no beam search)
 def generate_caption(image):
     if image.mode != "RGB":
         image = image.convert(mode="RGB")
     pixel_values = processor(images=image, return_tensors="pt").pixel_values
-    output_ids = model.generate(pixel_values, max_length=16, num_beams=4)
+    output_ids = model.generate(pixel_values, max_length=16)
     caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
     return caption
+
+
 
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
     image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     with st.spinner("Generating caption..."):
         caption = generate_caption(image)
         st.success("Caption Generated!")
         st.markdown(f"**Caption:** _{caption}_")
 
+        # Option to download caption
+        st.download_button(
+            label="📄 Download Caption as .txt",
+            data=caption,
+            file_name="caption.txt",
+            mime="text/plain"
+        )
